ee28dacaf1683c105cae0fd461b4fc4065504305
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2023-12-21 12:10:36 +0630

    error fixed

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const userCtrl = require('./controllers/users.ctrl'); //user ctrl
+const userCtrl = require('../controllers/users.ctrl'); //user ctrl
 
 const usersRouter = express.Router();
 
-usersRouter.route("/").get(userCtrl)
+usersRouter.route("/").get(userCtrl.getAllUsers)
 
-usersRouter.route("/login").post(userCtrl.)
+// usersRouter.route("/login").post(userCtrl.getAllUsers)
 
 
 module.exports = usersRouter;
